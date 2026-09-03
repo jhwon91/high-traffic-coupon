5f32319cb001996b64c28e96ff2c1a816b69c2dd
wjh <jwon3991.9191@gmail.com>
2026-09-03 22:39:22 +0900

    first commit

---
+++ src/test/java/com/apiece/coupon/CouponApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apiece.coupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CouponApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
